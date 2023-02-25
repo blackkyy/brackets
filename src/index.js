6f1ba12d3a4dc18d90c9b04b2568a373fd8ec0d7
Amal <yakubov.amal@gmail.com>
2023-02-25 16:31:40 +0500

    feat: finished task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+
+    // Find the bracket configuration that matches the current character
+    const bracketConfig = bracketsConfig.find(
+      ([open, close]) => open === char || close === char
+    );
+
+    if (bracketConfig) {
+      const [open, close] = bracketConfig;
+
+      // If the current character is an opening bracket, push it onto the stack
+      if (char === open) {
+        // Special case: opening and closing bracket can be the same
+        if (open === close && stack[stack.length - 1] === open) {
+          stack.pop();
+        } else {
+          stack.push(char);
+        }
+      }
+
+      // If the current character is a closing bracket, pop the top element from the stack
+      else {
+        const lastBracket = stack.pop();
+
+        // Check that the top element of the stack matches the current closing bracket
+        if (lastBracket !== open) {
+          return false;
+        }
+      }
+    }
+  }
+
+  // If the stack is empty, the brackets are balanced
+  return stack.length === 0;
+};
